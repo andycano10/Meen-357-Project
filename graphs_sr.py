8a4a5eca74ad9dff5cbb131dbe96f56cd2d3fe89
ctfossati26 <ctfossati26@tamu.edu>
2024-09-17 21:42:26 -0500

    final motor graphs

--- graphs_sr.py
+++ graphs_sr.py
@@ -9,7 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from subfunctions import *
 
-#plot 1: w_out (omega/Ng) speed  vs. t_out =t/Ng
 r = get_gear_ratio(speed_reducer)
 
 
@@ -31,17 +30,36 @@ for i in range(len(t)):
     new = t[i]*r
     tau_out.append(new)
 
-#plt.plot(tau_out, wout)
- 
-#GRAPH 2:t_out vs. Pout [Nm] (use torque on the x-axis)
-P = tau*omega
-plt.plot(tau_dcmotor(omega, motor), P)
-plt.show()
 
+
+P = tau*omega
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(6,8))
+fig.suptitle('Graphs for the Speed Reducer')
+
+#plot 1: w_out (omega/Ng) speed  vs. t_out =t/Ng
+ax1.plot(tau_out, wout)
+ax1.set_title('Speed vs Torque')
+ax1.set_title('Speed vs Torque')
+ax1.set_xlabel('Reducer Torque [Nm]')
+ax1.set_ylabel('Reducer Speed [rad/s]')
+
+#GRAPH 2:t_out vs. Pout [Nm] (use torque on the x-axis)
+ax2.plot(tau_dcmotor(omega, motor),P)
+ax2.set_title('Power vs Torque')
+ax2.set_xlabel('Reduce Shaft Torque [Nm]')
+ax2.set_ylabel('Reducer Power [W]')
 
 
 #GRAPH 3: motor power [W] vs. motor shaft speed [rad/s] (use speed on the x-axis)
-plt.plot(wout, P)
+ax3.plot(wout, P)
+ax3.set_title('Power vs Speed')
+ax3.set_xlabel('Reducer Speed [rad/s]')
+ax3.set_ylabel('Reducer Power [W]')
+
+plt.tight_layout()
 plt.show()
+
+plt.tight_layout()
 
 
