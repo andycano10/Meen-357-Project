8bb4c9dce958a06452bab7b32448b4584e2eced9
andycano10 <andycano10@tamu.edu>
2024-10-11 12:35:22 -0500

    new

---
+++ rover_experiment1.py
@@ -0,0 +1,28 @@
+import subfunctions as sb
+import matplotlib.pyplot as plt
+
+sb.end_event['max_distance'] = 1000
+sb.end_event['max_time'] = 10000
+sb.end_event['min_velocity'] = 0.01
+
+rover = sb.simulate_rover(sb.rover, sb.planet, sb.experiment, sb.end_event)
+fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(6,8))
+fig.suptitle('Rover Trajectory Graphs')
+
+ax1.plot(rover['telemetry']['position'],rover['telemetry']['time'])
+ax1.set_title('Position vs Time')
+ax1.set_xlabel('Position [m]')
+ax1.set_ylabel('Time [s]')
+
+ax2.plot(rover['telemetry']['velocity'],rover['telemetry']['time'])
+ax2.set_title('Velocity vs Time')
+ax2.set_xlabel('Velocity [m/s]')
+ax1.set_ylabel('Time [s]')
+
+
+ax3.plot(rover['telemetry']['power'],rover['telemetry']['time'])
+ax3.set_title('Power vs Time')
+ax3.set_xlabel('Power [W]')
+ax1.set_ylabel('Time [s]')
+
+plt.tight_layout()
